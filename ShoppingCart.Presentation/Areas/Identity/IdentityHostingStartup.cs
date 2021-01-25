ffc507e657facd3e4c7ef58e4b1c99408cba7674
ryan.attard <ryan.attard@mcast.edu.mt>
2021-01-25 20:34:16 +0100

    Upgrading to 3.1.11 to fix problem with generating scaffolded classes

---
+++ ShoppingCart.Presentation/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ShoppingCart.Presentation.Data;
+
+[assembly: HostingStartup(typeof(ShoppingCart.Presentation.Areas.Identity.IdentityHostingStartup))]
+namespace ShoppingCart.Presentation.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- ShoppingCart.Presentation/Startup.cs
+++ ShoppingCart.Presentation/Startup.cs
@@ -48,6 +48,23 @@ namespace ShoppingCart.Presentation
             });
 
 
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Default Lockout settings.
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+            });
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
