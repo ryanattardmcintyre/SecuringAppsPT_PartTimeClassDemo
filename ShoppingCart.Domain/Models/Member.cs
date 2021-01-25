b8d7bf84bcfaefce1a23933bf4a2ec41d519d91a
ryan.attard <ryan.attard@mcast.edu.mt>
2021-01-25 19:43:29 +0100

    Committing before upgrading library to 3.1.10

--- ShoppingCart.Domain/Models/Member.cs
+++ ShoppingCart.Domain/Models/Member.cs
@@ -11,5 +11,9 @@ namespace ShoppingCart.Domain.Models
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Mobile { get; set; }
     }
 }
--- ShoppingCart.Presentation/Startup.cs
+++ ShoppingCart.Presentation/Startup.cs
@@ -40,7 +40,14 @@ namespace ShoppingCart.Presentation
 
             DependencyContainer.RegisterServices(services,
                   Configuration.GetConnectionString("DefaultConnection"));
-            
+
+            services.AddAuthentication().AddMicrosoftAccount(microsoftOptions =>
+            {
+                microsoftOptions.ClientId = Configuration["Authentication:Microsoft:ClientId"];
+                microsoftOptions.ClientSecret = Configuration["Authentication:Microsoft:ClientSecret"];
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
